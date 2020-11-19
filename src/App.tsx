bf72632727bf564b108546ea5c845edabf079e34
Avni Kothari <avni.kothari@edovo.com>
2020-11-19 16:44:38 -0600

    first image

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import testImageOne from './images/test-image-1.jpg'
 
 function App() {
   return (
     <div className="App">
+      <br />
+      <img alt="" src={testImageOne} height="600" width="400"/>
       <p>
         Photos by Matt Wener
       </p>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
